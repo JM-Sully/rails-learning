7ee5eadd27d728e7c0d3d162638f2c43e1fe48dd
JM-Sully <jess.marie.sullivan@gmail.com>
2020-10-06 09:47:53 +0100

    adds validation for image URL type

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -11,7 +11,7 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product must be equal" do
-    product = Product.new(title: "My book", description: "yyy", image_url: z.jpeg)
+    product = Product.new(title: "My book", description: "yyy", image_url: "zzz.jpeg")
     product.price = -1
     assert product.invalid?
     assert_equal ['must be greater than or equal to 0.01'],
@@ -24,6 +24,27 @@ class ProductTest < ActiveSupport::TestCase
 
     product.price = 1
     assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title: "My book", description: "yyy", price: 1, image_url: image_url)
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpeg fred.png FRED.JPG FRED.JPG
+            http://a.b.c/x/y/z/fred.gif}
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |image_url|
+      assert new_product(image_url).valid?,
+            "#{image_url} shouldn't be invalid"
+    end
+
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?
+            "#{image_url} shouldn't be valid"
+    end
+
   end
 
 end

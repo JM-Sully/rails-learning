ba286fc12d482a20badf86728b3f49faea7efc27
JM-Sully <jess.marie.sullivan@gmail.com>
2020-10-15 07:40:59 +0100

    creates cart

---
+++ app/controllers/concerns/current_cart.rb
@@ -0,0 +1,11 @@
+module CurrentCart
+
+    private
+
+        def set_cart
+            @cart = Cart.fin(session[cart_id])
+        rescue ActiveRecord::RecordNptFound
+            @cart =Cart.create
+            session[:cart_id] =@cart.cart.cart.id
+        end
+    end

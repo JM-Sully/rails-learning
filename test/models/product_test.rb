5a2e0461127ee5ad4eb6500792abed40f87cbe9e
JM-Sully <jess.marie.sullivan@gmail.com>
2020-12-07 09:03:43 +0000

    adds unit tests to models

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -50,6 +52,15 @@ class ProductTest < ActiveSupport::TestCase
       assert new_product(image_url).invalid?
           "#{image_url} shouldn't be valid"
     end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title:        products(:ruby).title, #links to the fixture named ruby
+                          description:  "This is a book...",
+                          price:        1,
+                          image_url:    "fred.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
   end
 
 end

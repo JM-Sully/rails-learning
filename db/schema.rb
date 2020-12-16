87f5ffdc59e19cc6b89652d15aec72010e61ae68
JM-Sully <jess.marie.sullivan@gmail.com>
2020-12-16 08:06:25 +0000

    creates cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_182101) do
+ActiveRecord::Schema.define(version: 2020_12_16_074528) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
